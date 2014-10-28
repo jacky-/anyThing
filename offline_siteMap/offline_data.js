1cdbf3697c6b12c18afa4dfadbbc2e1688d6a7c5
zheng_j <zheng_j@Ctrip.com>
2014-10-28 10:59:08 +0800

    new temple

--- offline_siteMap/offline_data.js
+++ offline_siteMap/offline_data.js
@@ -1,94 +1,293 @@
-window.offline_prototypeData2 = {
-	"comment_review": {
-		"navName": "游记转评论审核系统",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_comment_review",
-		"imgSrc": "offline_comment_review.png"
-	}
-	,"review_user_pic": {
-		"navName": "酒店相册系统",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/hotels_pic/review_user_pic.php",
-		"imgSrc": "review_user_pic.png"
-	}
-	,"offline_group_buying": {
-		"navName": "团购业务员系统",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_group_buying/index.html",
-		"imgSrc": "offline_group_buying.png"
-	}
-	,"offline_hotel_order": {
-		"navName": "酒店订单系统",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/hotel_list.php",
-		"imgSrc": "offline_hotel_order.png"
-	}
-	,"offline_hotelint_platform": {
-		"navName": "预支付单明细",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotelint_platform/prepayment.html",
-		"imgSrc": "offline_hotelint_platform.png"
-	}
-	,"offline_incoming_call": {
-		"navName": "电话呼入",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_incoming_call/offline.php",
-		"imgSrc": "offline_incoming_call.png"
-	}
-	,"offline_inn": {
-		"navName": "修改客栈信息",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_inn/inn_info.php",
-		"imgSrc": "offline_inn.png"
-	}
-	,"offline_insurance": {
-		"navName": "offline_insurance",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_insurance/index.php",
-		"imgSrc": "offline_insurance.png"
-	}
-	,"offline_order20120605": {
-		"navName": "offline酒店预订",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_order20120605/hotel.php",
-		"imgSrc": "offline_order20120605.png"
-	}
-	,"offline_ResHotelInt_platform": {
-		"navName": "海外酒店管理平台",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_ResHotelInt_platform/index.html",
-		"imgSrc": "offline_ResHotelInt_platform.png"
-	}
-	,"offline_seize": {
-		"navName": "拍拍促销平台",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/seize_index.php",
-		"imgSrc": "offline_seize.png"
-	}
-	,"offline_seize": {
-		"navName": "拍拍促销平台",
-		"path": "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/seize_index.php",
-		"imgSrc": "offline_seize.png"
-	}
-}
-
 window.offline_prototypeData = {
-	offline_seize: {
-		sysName: "拍拍促销平台",
-		PRUD_URL: "asd",
-		pages: {
+	comment_review: {
+		sysName: "游记转评论审核系统",
+		PRUD_URL: "",
+		account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
 			offline_seize_index: {
 				pageName: "首页",
 				imgSrc: "./imgs/offline_comment_review.png",
-				ui_URL: "",
-				dev_URL: ""
-			}
-			,offline_seex: {
-				pageName: "首页",
-				imgSrc: "./imgs/offline_seize.png",
-				ui_URL: "",
-				dev_URL: ""
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_comment_review",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_comment_review"
 			}
 		}
 	}
-	,offline_seie: {
-		sysName: "拍拍销平台",
-		PRUD_URL: "",
-		pages: {
-			offline_seize_index: {
+	,offline_group_buying: {
+		sysName: "团购业务员系统",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_group_buying_index: {
 				pageName: "首页",
-				imgSrc: "",
-				ui_URL: "",
-				dev_URL: ""
+				imgSrc: "./imgs/offline_group_buying.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_group_buying/index.html",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_group_buying/index.html"
+			}
+		}
+	}
+	,offline_hotel_order: {
+		sysName: "酒店订单系统",
+		PRUD_URL: "",
+		account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_hotel_order_hotel_list: {
+				pageName: "酒店列表页",
+				imgSrc: "./imgs/offline_hotel_order_hotel_list.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/hotel_list.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/hotel_list.php"
+			}
+			,offline_hotel_order_hotel_search: {
+				pageName: "浏览订单状态",
+				imgSrc: "./imgs/offline_hotel_order_hotel_search.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/hotel_search.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/hotel_search.php"
+			}
+			,offline_hotel_order_notice_delivery: {
+				pageName: "notice_delivery",
+				imgSrc: "./imgs/offline_hotel_order_notice_delivery.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/notice_delivery.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/notice_delivery.php"
+			}
+			,offline_hotel_order_offline1595: {
+				pageName: "宾馆点评反馈管理-点评列表",
+				imgSrc: "./imgs/offline_hotel_order_offline1595.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/offline1595.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/offline1595.php"
+			}
+			,offline_hotel_order_offline5681: {
+				pageName: "酒店发票－发票地址信息维护",
+				imgSrc: "./imgs/offline_hotel_order_offline5681.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/offline5681.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/offline5681.php"
+			}
+			,offline_hotel_order_order_detail: {
+				pageName: "浏览订单状态",
+				imgSrc: "./imgs/offline_hotel_order_order_detail.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/order_detail.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/order_detail.php"
+			}
+			,offline_hotel_order_order_edit: {
+				pageName: "order_edit",
+				imgSrc: "./imgs/offline_hotel_order_order_edit.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/order_edit.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/order_edit.php"
+			}
+			,offline_hotel_order_order_list: {
+				pageName: "order_list",
+				imgSrc: "./imgs/offline_hotel_order_order_list.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/order_list.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/order_list.php"
+			}
+			,offline_hotel_order_order_list_new: {
+				pageName: "order_list_new",
+				imgSrc: "./imgs/offline_hotel_order_order_list_new.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/order_list_new.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/order_list_new.php"
+			}
+			,offline_hotel_order_order_record: {
+				pageName: "order_record",
+				imgSrc: "./imgs/offline_hotel_order_order_record.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/order_record.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/order_record.php"
+			}
+			,offline_hotel_order_review_index: {
+				pageName: "review_index",
+				imgSrc: "./imgs/offline_hotel_order_review_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/review_index.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/review_index.php"
+			}
+			,offline_hotel_order_review_log: {
+				pageName: "敏感词审核日志",
+				imgSrc: "./imgs/offline_hotel_order_review_log.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/review_log.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/review_log.php"
+			}
+			,offline_hotel_order_review_pic: {
+				pageName: "宾馆点评反馈管理－点评列表（国内）",
+				imgSrc: "./imgs/offline_hotel_order_review_pic.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/review_pic.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/review_pic.php"
+			}
+			,offline_hotel_order_review_word: {
+				pageName: "敏感词审核模块",
+				imgSrc: "./imgs/offline_hotel_order_review_word.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotel_order/review_word.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotel_order/review_word.php"
+			}
+		}
+	}
+	,offline_hotelint_platform: {
+		sysName: "担保订单支付明细",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_hotelint_platform_assure: {
+				pageName: "assure",
+				imgSrc: "./imgs/offline_hotelint_platform_assure.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotelint_platform/assure.html",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotelint_platform/assure.html"
+			}
+			,offline_hotelint_platform_prepayment: {
+				pageName: "prepayment",
+				imgSrc: "./imgs/offline_hotelint_platform_prepayment.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hotelint_platform/prepayment.html",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hotelint_platform/prepayment.html"
+			}
+		}
+	}
+	,offline_hoteltool: {
+		sysName: "酒店工具聚合",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_hoteltool_index: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_hotelint_platform_assure_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_hoteltool/index.html",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_hoteltool/index.html"
+			}
+		}
+	}
+	,offline_incoming_call: {
+		sysName: "电话呼入",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_incoming_call_index: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_incoming_call_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_incoming_call/offline.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_incoming_call/offline.php"
+			}
+		}
+	}
+	,offline_inn: {
+		sysName: "修改客栈信息",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_inn_inn_info: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_inn_inn_info.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_inn/inn_info.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_inn/inn_info.php"
+			}
+		}
+	}
+	,offline_insurance: {
+		sysName: "保险录入",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_insurance_index: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_insurance_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_insurance/index.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_insurance/index.php"
+			}
+		}
+		,pages: {
+			offline_insurance_edit: {
+				pageName: "保险录入",
+				imgSrc: "./imgs/offline_insurance_edit.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_insurance/edit.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_insurance/edit.php"
+			}
+		}
+	}
+	,offline_order20120605: {
+		sysName: "offline_order20120605",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_order20120605_hotel: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_order20120605_hotel.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_order20120605/hotel.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_order20120605/hotel.php"
+			}
+		}
+	}
+	,offline_ResHotelInt_platform: {
+		sysName: "海外酒店管理平台",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_ResHotelInt_platform_index: {
+				pageName: "首页",
+				imgSrc: "./imgs/offline_ResHotelInt_platform_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_ResHotelInt_platform/index.html",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_ResHotelInt_platform/index.html"
+			}
+		}
+	}
+	,offline_seize: {
+		sysName: "海外酒店促销信息配置",
+		PRUD_URL: ""
+		,account:{
+			userName:""
+			,password:""
+		}
+		,pages: {
+			offline_seize_promotion: {
+				pageName: "拍拍促销模块promotion",
+				imgSrc: "./imgs/offline_seize_promotion.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/promotion.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_seize/promotion.php"
+			}
+			,offline_seize_seize_detail: {
+				pageName: "拍拍促销模块seize_detail",
+				imgSrc: "./imgs/offline_seize_seize_detail.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/seize_detail.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_seize/seize_detail.php"
+			}
+			,offline_seize_seize_edit: {
+				pageName: "拍拍促销模块seize_edit",
+				imgSrc: "./imgs/offline_seize_seize_edit.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/seize_edit.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_seize/seize_edit.php"
+			}
+			,offline_seize_seize_index: {
+				pageName: "拍拍促销模块seize_index",
+				imgSrc: "./imgs/offline_seize_seize_index.png",
+				ui_URL: "http://prototype.ui.sh.ctriptravel.com/code_packet/offline_seize/seize_index.php",
+				dev_URL: "http://prototype.dev.sh.ctriptravel.com/code_packet/offline_seize/seize_index.php"
+			}
+			,offline_seize_seize_monitor: {
+				pageName: "拍拍促销模块seize_monitor",
+				imgSrc: "./imgs/offline_seize_seize_monitor.png",
+				ui_URL: "http://prototype.local.sh.ctriptravel.com/code_packet/offline_seize/seize_monitor.php",
+				dev_URL: "http://prototype.local.sh.ctriptravel.com/code_packet/offline_seize/seize_monitor.php"
 			}
 		}
 	}
