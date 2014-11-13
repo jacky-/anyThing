6dd1aaf584509afafc6d92594434b609f1a19ad2
zheng_j <zheng_j@Ctrip.com>
2014-11-13 14:03:08 +0800

    .

---
+++ offline_base_styles/side_nav.php
@@ -0,0 +1,9 @@
+<div id="side_nav" class="side_nav">
+	<h1 class="side_nav-title">UI-offline</h1>
+	<nav class="side_nav_list">
+		<a href="#">button</a>
+		<a href="#">button</a>
+		<a href="#">button</a>
+		<a href="#">button</a>
+	</nav>
+</div>
