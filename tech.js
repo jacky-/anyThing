57cf5fe8ce42de07d2bd14c6449277f572fb67cc
郑俊 <jackyzheng@anjuke.com>
2014-11-30 15:18:10 +0800

    tt

---
+++ tech.js
@@ -0,0 +1,39 @@
+/**
+ * 一个匿名函数
+ */
+(function(){
+	var _myName = 'ctripUED';
+	//do sth
+	console.log(_myName)
+})
+
+/**
+ * 一个简单的闭包
+ */
+var closure1 = (function(){
+	var _myName = 'ctrip';
+	return function(dept){
+		_myName = _myName + dept
+		console.log(_myName);
+	}
+})();
+
+closure1(UED);//'ctripUED'
+
+/**
+ * 一个简单闭包实现缓存
+ * @return {Object}
+ */
+var closure_Cache = (function(){
+	var _cache = {};
+	return {
+		getInfo:function(name){
+			_cache[name] = _cache[name]||{
+				comp:'ctrip',
+				employees:40
+			};
+			return _cache[name]
+		}
+	}
+})();
+closure_Cache.getInfo('UED')
