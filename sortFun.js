265855704772441edf253918e81af2de22928dfd
Jacky Zheng <zheng_j@Ctrip.com>
2015-02-03 13:33:09 +0800

    mdf

--- sortFun.js
+++ sortFun.js
@@ -1,4 +1,5 @@
 Array.prototype.insertSort = function(fn) {
+	// 稳定
 	var array = this;
 	var fn = fn || function(a, b) {
 		return b > a
@@ -17,6 +18,7 @@ Array.prototype.insertSort = function(fn) {
 	return array;
 }
 Array.prototype.binaryInsertionSort = function(fn) {
+	// 稳定
 	var array = this;
 	var fn = fn || function(a, b) {
 		return b > a
@@ -27,7 +29,7 @@ Array.prototype.binaryInsertionSort = function(fn) {
 			right = i - 1;
 		while (left <= right) {
 			var middle = parseInt((left + right) / 2);
-			if (fn(key, array[middle])) {
+			if (fn(array[middle],key)) {
 				right = middle - 1;
 			} else {
 				left = middle + 1;
@@ -50,7 +52,7 @@ Array.prototype.selectionSort = function(fn) {
 	for (var i = 0; i < array.length - 1; i++) {
 		var min = array[i];
 		for (var j = i + 1; j < array.length; j++) {
-			if (fn(array[j], min)) {
+			if (fn(min,array[j])) {
 				temp = min;
 				min = array[j];
 				array[j] = temp;
@@ -82,6 +84,7 @@ Array.prototype.quickSort = function(fn) {
 	return Array.prototype.quickSort(left).concat([pivot], Array.prototype.quickSort(right));
 }
 Array.prototype.shellSort = function(fn) {
+	//不稳定
 	var array = this;
 	var fn = fn || function(a, b) {
 		return b > a
@@ -109,7 +112,7 @@ Array.prototype.shellSort = function(fn) {
 				tem = f = step * j + i;
 				key = array[f];
 				while ((tem -= step) >= 0) { // 依次向上查找
-					if (fn(key,array[tem])) {
+					if (fn(array[tem],key)) {
 						array[tem + step] = array[tem];
 					} else {
 						break;
@@ -123,6 +126,7 @@ Array.prototype.shellSort = function(fn) {
 
 }
 Array.prototype.qSort = function(fn) {
+	// 不稳定
 	var arr = this;
 	var fn = fn || function(a, b) {
 		return b > a
@@ -134,9 +138,9 @@ Array.prototype.qSort = function(fn) {
           while(true){
           //大的放到右边，小的放到左边, i与j均为游标
             // while(arr[++i]<mid);
-            while(fn(arr[++i],mid));
+            while(fn(mid,arr[++i]));
             // while(arr[--j]>mid);
-            while(fn(mid,arr[--j]));
+            while(fn(arr[--j],mid));
             if(i>=j)break;//判断条件
             var temp = arr[i];
             arr[i]=arr[j];
@@ -149,6 +153,7 @@ Array.prototype.qSort = function(fn) {
    }
 }
 Array.prototype.bubbleSort = function(fn) {
+	// 稳定
 	var array = this;
 	var fn = fn || function(a, b) {
 		return b > a
@@ -156,7 +161,7 @@ Array.prototype.bubbleSort = function(fn) {
 	for (var i = 0; i < array.length; i++) {
 		for (var j = array.length-1; j > 0; j--) {
 			// if (array[j] < array[j - 1]) {
-			if (fn(array[j],array[j - 1])) {
+			if (fn(array[j - 1],array[j])) {
 				var temp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = temp;
